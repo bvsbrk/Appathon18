6cc50f9ef68296c329271451b59b28a4528bcf37
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2018-03-30 16:11:10 +0530

    Icons added

--- app/src/main/java/bk/appathon18/MainActivity.java
+++ app/src/main/java/bk/appathon18/MainActivity.java
@@ -1,13 +1,51 @@
 package bk.appathon18;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.widget.Toast;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+
+
+    GoogleMap googleMap;
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.google_map_view);
+        mapView.onCreate(null);
+        mapView.onResume();
+        mapView.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(this);
+        this.googleMap = googleMap;
+        googleMap.setOnMapClickListener(this);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        } else {
+            CameraUpdate cameraPosition = CameraUpdateFactory.newLatLngZoom((new LatLng(12.970537, 79.159960)), 17);
+            googleMap.moveCamera(cameraPosition);
+        }
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Toast.makeText(this, latLng.latitude + " " + latLng.longitude, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+    }
+
 }
