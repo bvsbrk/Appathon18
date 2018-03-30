fb9e1a38a12653b1a2cdb58bb563bc370a84b674
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2018-03-30 22:54:53 +0530

    Map Listeners added

--- app/src/main/java/bk/appathon18/MainActivity.java
+++ app/src/main/java/bk/appathon18/MainActivity.java
@@ -1,14 +1,24 @@
 package bk.appathon18;
 
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnCameraMoveStartedListener {
 
@@ -16,12 +26,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     GoogleMap googleMap;
     MapView mapView;
     Button navigateButton;
+    List<Address> addresses;
+    Geocoder geocoder;
+    LinearLayout bottomSheetLayout;
+    BottomSheetBehavior sheetBehavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomSheetLayout = findViewById(R.id.bottom_sheet_distress);
+        sheetBehavior = BottomSheetBehavior.from(bottomSheetLayout);
+        setUpbottomSheet();
+        geocoder = new Geocoder(this);
         navigateButton = findViewById(R.id.navigateButton);
         mapView = findViewById(R.id.google_map_view);
         mapView.onCreate(null);
@@ -29,10 +47,44 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView.getMapAsync(this);
     }
 
+    private void setUpbottomSheet() {
+        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN: {
+                        break;
+                    }
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+                        break;
+                    }
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                        break;
+                    }
+                    case BottomSheetBehavior.STATE_DRAGGING: {
+                        break;
+                    }
+                    case BottomSheetBehavior.STATE_SETTLING: {
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+
+
+        });
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         MapsInitializer.initialize(this);
         this.googleMap = googleMap;
+        this.googleMap.setMyLocationEnabled(true);
+        showMyLocationButton();
         googleMap.setOnMapClickListener(this);
         googleMap.setOnCameraMoveStartedListener(this);
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
@@ -46,15 +98,33 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+    private void showMyLocationButton() {
+        View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+        locationButton.setVisibility(View.GONE);
+    }
+
     @Override
     public void onMapClick(LatLng latLng) {
+        googleMap.clear();
         navigateButton.animate().alpha(1f).translationY(-100).setDuration(300);
+        bottomSheetLayout.animate().alpha(0f).setDuration(300);
+        MarkerOptions currentLocationMarker = new MarkerOptions();
+        currentLocationMarker.position(latLng);
+        try {
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+        } catch (Exception ignored) {
+        }
+        if (addresses.size() != 0) {
+            currentLocationMarker.title(addresses.get(0).getFeatureName());
+        }
+        Marker marker = googleMap.addMarker(currentLocationMarker);
+        marker.showInfoWindow();
     }
 
     public void navigateClick(View view) {
         float alpha = view.getAlpha();
         if (alpha != 0f) {
-
+            Toast.makeText(this, "alpha", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -62,5 +132,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onCameraMoveStarted(int i) {
         navigateButton.animate().alpha(0f).translationY(100).setDuration(300);
+        bottomSheetLayout.animate().alpha(1f).setDuration(300);
     }
 }
